be9992e8f3983aa87d5792da06dc0311d59c6cfe
Carlos Ramirez <10344966@my.uvu.edu>
2021-01-27 02:21:19 -0700

    Sidescroller game functional

---
+++ SideScroller/Assets/Scripts/ballGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballGenerator : MonoBehaviour
+{
+    public GameObject Ball;
+    int counter = 0;
+
+    IEnumerator Start()
+    {
+        while (counter < 25)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(Ball);
+            counter++;
+        }
+    }
+}
