4356ab11f7de267f6542dc301e686700f1d44b22
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-01 22:30:05 -0700

    Started script to add user control

---
+++ SideScroller/Assets/Scripts/UserInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody ball;
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ball.AddForce(direction, ForceMode2D.Force);
+        }
+    }
+}
