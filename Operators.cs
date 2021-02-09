20bfe3c6d0e089762f8c289141177a6381f07d78
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-08 23:43:53 -0700

    Started on Variables and Operators Programs

---
+++ Operators.cs
@@ -0,0 +1,14 @@
+using System;
+using Variables.cs;
+
+namespace Operators
+{
+    void main()
+    {
+        Console.WriteLine("Integer value is: " + myNum);
+        Console.WriteLine("Double value is: " + doubleNum);
+        Console.WriteLine("Char value is: " + letter);
+        Console.WriteLine("String value is: " + word);
+        Console.WriteLine("Bool value is: " + headsUp);
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Variables
+{
+    void main ()
+    {
+        int myNum = 50;
+        double doubleNum = -29.99;
+        char letter = 'b';
+        string word = "Hello World";
+        bool headsUp = true;
+
+        Console.WriteLine("Integer value is: " + myNum);
+        Console.WriteLine("Double value is: " + doubleNum);
+        Console.WriteLine("Char value is: " + letter);
+        Console.WriteLine("String value is: " + word);
+        Console.WriteLine("Bool value is: " + headsUp);
+    }
+}
