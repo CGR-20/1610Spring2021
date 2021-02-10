12f44d3b68486a52628fa0d5dfd809a1ac69d81f
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-09 17:49:46 -0700

    Updated Variables and Operators Scripts
    I feel silly for typing this, but does the scripts have to actually run? I can't for the life of me run .cs files in Visual Studio.

--- Operators.cs
+++ Operators.cs
@@ -1,10 +1,30 @@
 using System;
-using Variables.cs;
 
 namespace Operators
 {
     void main()
     {
+        int myNum = 50;
+        double doubleNum = -29.99;
+        char letter = 'b';
+        string word = "Hello World";
+        bool headsUp = true;
+
+        Console.WriteLine("Integer value is: " + myNum);
+        Console.WriteLine("Double value is: " + doubleNum);
+        Console.WriteLine("Char value is: " + letter);
+        Console.WriteLine("String value is: " + word);
+        Console.WriteLine("Bool value is: " + headsUp);
+
+        myNum = 25 - 3;
+        doubleNum = doubleNum / 6.29;
+        letter = 'z';
+        word = "Goodbye World";
+        headsUp = false;
+
+        if (doubleNum < -5)
+            doubleNum = -10;
+
         Console.WriteLine("Integer value is: " + myNum);
         Console.WriteLine("Double value is: " + doubleNum);
         Console.WriteLine("Char value is: " + letter);
