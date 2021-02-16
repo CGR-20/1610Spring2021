33f11a0812435d60bd1ca74d54bd42f0246ca678
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-15 23:36:25 -0700

    Finished Conditional and Switch Scripts
    Also added screenshots.
    I also decided to turn in future scripts in my Unity sandbox project, to make scripting more easier (hopefully).

---
+++ Beginner_Scripting_3D/Assets/Scripts/Practice Scripts/Conditionals.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    void Start()
+    {
+        int x = 50;
+        int y = 10;
+
+        if (x > y)
+            Debug.Log("Hello World");
+        else if (x < y)
+            Debug.Log("What's Up World?");
+        else
+            Debug.Log("Goodbye World");
+    }
+}
---
+++ Beginner_Scripting_3D/Assets/Scripts/Practice Scripts/Switches.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    void Start()
+    {
+        string word = "Soda";
+
+        switch(word)
+        {
+            case "Juice":
+                Debug.Log("Your drink is juice");
+                break;
+            case "Beer":
+                Debug.Log("Your drink is for men");
+                break;
+            case "Soda":
+                Debug.Log("Your drink is delicious");
+                break;
+            default:
+                Debug.Log("Your drink is either something else or nonexistent");
+                break;
+        }
+    }
+}
