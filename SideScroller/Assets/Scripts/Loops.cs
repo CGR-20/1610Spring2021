f0bd1dd43a13dae70a6702137e601a65fcbcce76
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-22 18:59:02 -0700

    Added Lord of the Rings reference to game project

---
+++ SideScroller/Assets/Scripts/Loops.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int number, total;
+    public Weapon[] weapons;
+
+    void Start()
+    {
+        /*
+        for (int i = 0; i < number; i++)
+        {
+            total = i;
+            print(total);
+        }
+        */
+
+        foreach (var weapon in weapons)
+            print(message: "You have my " + weapon.name);
+    }
+}
