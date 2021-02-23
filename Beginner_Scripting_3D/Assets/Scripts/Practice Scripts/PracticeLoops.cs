0a75de0db326bb850e9120789e97234a7b469b9d
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-23 15:30:00 -0700

    Started on for loop practice scripts
    Also added screenshot

---
+++ Beginner_Scripting_3D/Assets/Scripts/Practice Scripts/PracticeLoops.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeLoops : MonoBehaviour
+{
+    int counter = 0;
+    void Start()
+    {
+        while (counter < 5)
+        {
+            print("Counter is at: " + counter);
+            counter++;
+        }
+    }
+}
