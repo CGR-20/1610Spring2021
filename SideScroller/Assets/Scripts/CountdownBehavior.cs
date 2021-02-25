aba577fc30ab0963b0227608539e01f69bfea83d
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-24 21:35:49 -0700

    Added countdown timer

---
+++ SideScroller/Assets/Scripts/CountdownBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownBehavior : MonoBehaviour
+{
+    public int maxNumber = 4;
+    public float holdTime = 1f;
+    public Text countDownText;
+
+    private WaitForSeconds wfsObj;
+    public int number = 3;
+
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(holdTime);
+
+        while (number > 0)
+        {
+            countDownText.text = number.ToString();
+            number--;
+            yield return wfsObj;
+        }
+        countDownText.text = "GO!";
+        yield return wfsObj;
+        countDownText.text = null;
+    }
+}
