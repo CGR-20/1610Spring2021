88069fb1e77d6b449fe37bdf6dee2c002ae53b96
Carlos Ramirez <10344966@my.uvu.edu>
2021-02-26 15:52:04 -0700

    Added a more complex loop

--- Beginner_Scripting_3D/Assets/Scripts/Practice Scripts/PracticeLoops.cs
+++ Beginner_Scripting_3D/Assets/Scripts/Practice Scripts/PracticeLoops.cs
@@ -6,8 +6,10 @@ public class PracticeLoops : MonoBehaviour
 {
     int counter = 0;
     string[] names = { "Carlos", "Steve", "Alex" };
+    int[] numList = new int[5];
     void Start()
     {
+        /*
         while (counter < 5)
         {
             print("Counter is at: " + counter);
@@ -16,5 +18,12 @@ public class PracticeLoops : MonoBehaviour
 
         foreach (string i in names)
             print("Hello, my name is " + i);
+        */
+
+        for (int i = 0; i < 5; i++)
+            numList[i] = i;
+
+        foreach (int i in numList)
+            print(numList[i]);
     }
 }
