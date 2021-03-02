98b34f3161c213b5425769553248db8e740e7352
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-02 16:47:29 -0700

    Finished 1.2 and 1.3

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player; // allows us to tell the script to focus on the vehicle
+    private Vector3 offset = new Vector3(0, 10, -12); // offsets camera so it's not inside the vehicle
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset; // updates the camera
+        // position to follow the player, while also offseting the camera to be above and behind the player
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // Move the vehicle forward
+        //transform.Translate(0, 0, 1); // moves in the z axis by 1 each frame
+        transform.Translate(Vector3.forward * Time.deltaTime * speed); // easier since Vector3 is the 3D object
+        // Vector3.forward equals 0,0,1
+        // Time.deltaTime is equal to 1 m/s
+    }
+}
