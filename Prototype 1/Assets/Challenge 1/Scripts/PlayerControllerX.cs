f1fe9f020abfcab63ca0068874acb21376419ec4
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-05 18:38:54 -0700

    Added propeller spin script

--- Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 20.0f;
-    public float rotationSpeed = 50.0f;
+    public float rotationSpeed = 100.0f;
     public float verticalInput;
 
     // Start is called before the first frame update
