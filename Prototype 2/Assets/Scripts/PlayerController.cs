d38c57722310438901ac957a2e7d7c3a7921eec0
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-09 17:49:35 -0700

    Finished 2.1

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float hInput;
-    // Start is called before the first frame update
+    public float hInput; // recieves user input in the horizontal direction
+    public float speed = 10.0f; // the speed at which the player moves
+    public float xRange = 10; // the horizontal range that the player can move in
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        // keep player in bounds
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
     }
 }
