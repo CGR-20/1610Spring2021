8049bd9cb52cbf8263f1c597d7ac2dbf236eb131
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-12 18:02:50 -0700

    Started 2.3

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs; // creates array that we can drag prefabs to in the inspector
+
+    void Update()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length); // generates random number between 0 and size of array
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), 
+                animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
