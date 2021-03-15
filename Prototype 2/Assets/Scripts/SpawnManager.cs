3e5e034ec52172ec6ebdf6c98e240566020f2a62
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-15 15:31:31 -0600

    Finished 2.3 and Started 2.4

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs; // creates array that we can drag prefabs to in the inspector
+    private float spawnRangeX = 20;
+    private float spawnPozZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
 
     void Update()
+    { 
+        
+    }
+
+    void SpawnRandomAnimal()
     {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPozZ);
         int animalIndex = Random.Range(0, animalPrefabs.Length); // generates random number between 0 and size of array
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), 
-                animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
