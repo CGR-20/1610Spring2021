7a0dcb063ccc495b1e000c9dbdeba41e45663031
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-19 23:08:00 -0600

    Finished Challenge 2
    Prevented player spamming by adding improvised timer/counter

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    float time = 1.0f; // starts off counter at 1 second
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        time = Time.deltaTime + time; // increases time linearly
+        // On spacebar press, send dog whenever one second have passed
+        if (Input.GetKeyDown(KeyCode.Space) && time > 1)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            time = 0;
         }
     }
 }
