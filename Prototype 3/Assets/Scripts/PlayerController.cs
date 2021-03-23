21b40e99d2d1d4eb3539c6f9e5cbbbc75ecd3349
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-22 22:44:37 -0600

    Finished Prototype 3.1
    I decided to not stage everything in the project, gitkraken is having a hard time pushing it onto git

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
+    public bool gameOver;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>(); // gets rigid body of whatever the script is attached to
+        Physics.gravity *= gravityModifier; // changes gravity of game
+        isOnGround = true;
+        gameOver = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // makes player jump
+            isOnGround = false; // stops player from spamming jump
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // when player makes collision (ground), allow to jump again
+        if (collision.gameObject.CompareTag("Ground"))
+            isOnGround = true;
+        // when the player collides with obstacle, end game
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+    }
+}
---
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    void Start()
+    {
+        startPos = transform.position; // the bg's current position is it's start position
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // get accurate half x length of background
+    }
+
+    void Update()
+    {
+        // if the new bg position has passed a certain point on the left side, bring the bg back to the start
+        if (transform.position.x < startPos.x - repeatWidth)
+            transform.position = startPos;
+    }
+}
