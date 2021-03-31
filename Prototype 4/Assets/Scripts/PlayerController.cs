0e802f01fa0451d0bd53ba8793c2e51ccb5a095e
Carlos Ramirez <10344966@my.uvu.edu>
2021-03-30 20:11:50 -0600

    Finished 4.2

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerup;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        hasPowerup = false;
     }
 
     // Update is called once per frame
@@ -19,5 +21,22 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
